1f318bbd96473d825d7d1e1854b8257146a2ebb9
unknown <abhijit.x.roy@hotmail.com>
2024-11-23 23:09:26 +0530

    added alternative way to calculate age

--- Main.java
+++ Main.java
@@ -1,25 +1,23 @@
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
-import java.time.LocalDate;
-import java.time.Period;
-import java.time.format.DateTimeFormatter;
 
-public class Main{
-    public static void main(String[] args) {
-        
-        LocalDate curentDate = LocalDate.now();
-        LocalDate myDob = LocalDate.of(1989, 8, 02);
-        Period myPeriod = Period.between(myDob, curentDate);
+public class Main {
+	public static void main(String[] args) {
 
-        System.out.println("Age : "+ myPeriod.getDays() + " days, "+ myPeriod.getMonths() + " months and "+ myPeriod.getYears()+ " years.");
-        System.out.println("You are " + myPeriod.getYears()+ " years old.");
+		LocalDate curentDate = LocalDate.now();
+		// LocalDate myDob = LocalDate.of(1989, 8, 02);
+		// Period myPeriod = Period.between(myDob, curentDate);
 
-        // LocalDate customAge = LocalDate.parse("", DateTimeFormatter.ofPattern("dd-MM-yyyy"));
-        // Period customPeriod = Period.between(myDob, customAge);
-        // System.out.println("Age : "+ customPeriod.getDays() + " days, "+ customPeriod.getMonths() + " months and "+ customPeriod.getYears()+ " years.");
-        // System.out.println("You are : " + customPeriod.getYears()+ " years old.");
+		// System.out.println("Age : " + myPeriod.getYears() + " years, " + myPeriod.getMonths() + " months and "
+		// 		+ myPeriod.getDays() + " days.");
+		// System.out.println("You are " + myPeriod.getYears() + " years old.");
 
-    }
-} 
- 
+		LocalDate customDate = LocalDate.parse("02-08-1989", DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+		Period customPeriod = Period.between(customDate, curentDate);
+		System.out.println("Age : " + customPeriod.getYears() + " years, " + customPeriod.getMonths() + " months and "
+				+ customPeriod.getDays() + " days.");
+		System.out.println("You are : " + customPeriod.getYears() + " years old.");
+
+	}
+}
